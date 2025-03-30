53047ceebc5cb31ab338c332ace4fe4652263689
Taku1991 <Takuta1991@outlook.at>
2025-03-30 09:11:59 +0200

    Version änderung

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,10 +2,10 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Attribution = "https://genpkm.com";
+        public const string Attribution = "https://hideoutpk.de";
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v5.3.8";
+        public const string Version = "v1.0.1";
     }
 }
